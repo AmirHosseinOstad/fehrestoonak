59b94902af6f1fd766f35ed9b5d4728650a4ad02
AmirHosseinOstad <amirostad2021@hotmail.com>
2025-08-27 00:49:40 +0330

    Add method for fix text with spaces, on class.

---
+++ FixTextList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fehrestoonak_V1
+{
+    internal class FixTextList
+    {
+        public string FixSpaces(string Input, int RowLength)
+        {
+            string Output = "";
+
+            if (Input.Length <= RowLength)
+            {
+                string Spaces = "";
+                for (int i = 0; i < RowLength - Input.Length; i++)
+                {
+                    Spaces = Spaces + " ";
+                }
+                Output = Input + Spaces;
+            }
+            else
+            {
+                Output = Input.Substring(0, RowLength);
+            }
+
+            return Output;
+        }
+    }
+}
